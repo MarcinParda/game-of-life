e7f27b571ae07d389e587f6c23d49bf70ff45597
MarcinParda <marcin98parda@gmail.com>
2022-05-02 09:42:23 +0200

    added instruction on site

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,15 +7,18 @@ const App = () => {
     <div className="container">
       <h1 className="mb-2">Game of life</h1>
       <h4 className="mb-2">
+        Draw cell structure by clicking on cells in grid and hit "Start" to see
+        how they reproduce.
+      </h4>
+      <h4 className="mb-2">
         <a
           href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
           target="_blank"
           rel="noreferrer"
         >
-          How it works?
+          How algorithm works?
         </a>
       </h4>
-      <h3 className="mb-2">Game of life</h3>
       <GameGrid />
     </div>
   );
