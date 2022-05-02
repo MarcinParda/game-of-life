ca76efc790a3b7df43664f4a21f8acbf81478ea2
MarcinParda <marcin98parda@gmail.com>
2022-05-02 09:39:11 +0200

    add link to wikipedia

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,16 @@ const App = () => {
   return (
     <div className="container">
       <h1 className="mb-2">Game of life</h1>
+      <h4 className="mb-2">
+        <a
+          href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+          target="_blank"
+          rel="noreferrer"
+        >
+          How it works?
+        </a>
+      </h4>
+      <h3 className="mb-2">Game of life</h3>
       <GameGrid />
     </div>
   );
